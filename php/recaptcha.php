3eeddb4a2399785a1797fe9140308c0fdc1fb4d8
JoelFrias <fjoelfrias@gmail.com>
2021-03-24 14:28:58 -0400

    Joel

--- php/recaptcha.php
+++ php/recaptcha.php
@@ -7,7 +7,23 @@ if($recaptcha != ''){
     $var = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$recaptcha&remoteip=$ip");
     $array = json_decode($var, true);
     if($array['success']){
-       header ('location:guardarcitas.php');
+        
+        require 'conexion.php';
+
+        $Nombre = $_POST['Nombre'];
+        $Email = $_POST['Email'];
+        $Telefono = $_POST['Telefono'];
+        $Hora = $_POST['Hora'];
+        $Fecha = $_POST['Fecha'];
+        $Aseguradora = $_POST['Aseguradora'];
+        $Detalles = $_POST['Detalles'];
+        
+        mysqli_query($conexion, "INSERT INTO citas VALUES ('','$Nombre','$Email','$Telefono','$Fecha','$Hora','$Aseguradora','$Detalles')");
+
+        sleep(1);
+
+        header('location: ../invitado.php#citas');
+        
     }else{
        echo "Robot de mierda";
     }
