d1ecbbc966b140e34c5bf01311ed969beb28121c
JoelFrias <fjoelfrias@gmail.com>
2021-04-26 18:05:04 -0400

    Joel

--- paciente.php
+++ paciente.php
@@ -17,92 +17,12 @@
     <head>
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/menu.css">
+        <link rel="stylesheet" href="css/buscador.css">
         <script src="js/java.js"></script>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Consult Help</title>
         <link rel="shortcut icon" href="img/logo/icono.ico" type="image/x-icon">
-        <style>
-            .dataTables_filter input{
-             background-color: #fff;
-             border: 2px solid rgb(55, 122, 223);
-             margin: 20px;
-             border-radius: 8px;
-             height: 25px;
-             outline: none;
-            }
-
-            .dataTables_filter label{
-                position: relative;
-                left: 31%;
-                top: 10px;
-            }
-
-            .dataTables_length select{
-                width: auto;
-                display: inline-block;
-            }
-
-            .dataTables_length label{
-                position: relative;
-                top: 56px;
-                right: 33%;
-            }
-            .dataTables_info{
-                position: relative;
-                right: 34%;
-                top: 10px;
-            }
-                  
-            .paginate_button{
-                display: inline-block;
-                word-spacing: 0.20rem;
-            }
-            .pagination{
-               position: relative;
-               left: 34%;
-               border: 1px solid black;
-               border-radius: 5px;
-               width: 150px;
-               height: auto;
-            }
-                
-            .pagination li a{
-               color: black;
-               font-size: 11px
-            }
-
-            .pagination li{
-               margin: 3px;
-               padding: 2.5px;
-            }
-
-            .active{
-               background-color: rgb(55, 122, 223);
-               border-radius: 5px;
-               width: 15px
-            }
-
-            li.paginate_button.page-item{
-                border-left: 1px solid black;
-                border-right: 1px solid black;
-            }
-
-            li.paginate_button.page-item.previous{
-                border-left: 0px solid black;
-                border-right: 0px solid black;
-            }
-
-            li.paginate_button.page-item.next{
-                border-left: 0px solid black;
-                border-right: 0px solid black;
-            }
-
-            #mostrar-consultas h1{
-                position: relative;
-                top: 50px;
-            }
-        </style>
     </head>
     <body>
 
